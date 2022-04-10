ff7503992ea98908904605b4ab57c8c9e92aa25f
hi1210 <kungfusnake@gmail.com>
2022-04-10 06:38:17 -0700

    Add delay to donation page
    - Added a delay to donation page opening

--- src/ts/completed.ts
+++ src/ts/completed.ts
@@ -3,6 +3,10 @@ const donation = new URLSearchParams(location.search).get('donation')!;
 const amount = document.querySelector('#donation') as HTMLElement;
 amount.innerText = `$${donation}`;
 
-chrome.windows.create({
-    url: `https://donate.wck.org/give/393234#!/donation/checkout?donate=${donation}`,
-});
+setTimeout(
+    () =>
+        chrome.windows.create({
+            url: `https://donate.wck.org/give/393234#!/donation/checkout?donate=${donation}`,
+        }),
+    1000
+);
