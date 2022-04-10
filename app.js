8b81af95622c7623e637f8ce7f0787b69fe5b7d7
hi1210 <kungfusnake@gmail.com>
2022-04-09 23:25:15 -0700

    Create a content script for ebay
    - Content script for ebay that looks at the total cost
    - Remove app.js from index.html and popup.html

--- app.js
+++ app.js
@@ -1,3 +1,10 @@
 'use strict';
+var _a;
 const hello = 'Hello SLOtus Hacks!';
 console.log(hello);
+const total =
+    (_a = document.querySelector('[data-test-id=TOTAL]')) === null ||
+    _a === void 0
+        ? void 0
+        : _a.querySelector('.text-display');
+console.log(total.innerText);
